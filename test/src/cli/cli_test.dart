ac61a238be0c790e46dd25e092371aa311e75ffe
willow <willow.c.dev@gmail.com>
2026-01-11 20:09:53 +0900

    feat: refactor logger implementation in CLI tests to use shared test_helpers

--- test/src/cli/cli_test.dart
+++ test/src/cli/cli_test.dart
@@ -1,45 +1,7 @@
 import 'package:args/command_runner.dart';
 import 'package:locale_sheet/src/cli/cli.dart';
-import 'package:locale_sheet/src/cli/logger.dart';
 import 'package:test/test.dart';
-
-class TestLogger implements Logger {
-  final infos = <String>[];
-  final errors = <String>[];
-
-  @override
-  void info(String message) => infos.add(message);
-
-  @override
-  void error(String message) => errors.add(message);
-
-  @override
-  void infoOptions(Map<String, Object?> options) {
-    final joined = options.entries.map((e) => '${e.key}=${e.value}').join(', ');
-    infos.add('Options: $joined');
-  }
-
-  @override
-  void infoAvailableSheets(List<String> sheets) {
-    infos.add('Available sheets: ${sheets.join(', ')}');
-  }
-
-  @override
-  void infoSheetLocales(String sheetName, List<String> locales) {
-    infos.add('Sheet: $sheetName; Locales: ${locales.join(', ')}');
-  }
-
-  @override
-  void infoDefaultLocale(String locale) => infos.add('Default locale: $locale');
-
-  @override
-  void infoResult(String format, String outDir) => infos.add(
-    'Result: Success - "$format" format successfully written to $outDir.',
-  );
-
-  @override
-  void infoErrorResult(String message) => infos.add('Result: Error - $message');
-}
+import '../../test_helpers/logger.dart';
 
 void main() {
   group('ExportCommand', () {
--- test/src/cli/export_command_mock_test.dart
+++ test/src/cli/export_command_mock_test.dart
@@ -3,49 +3,12 @@ import 'dart:typed_data';
 
 import 'package:args/command_runner.dart';
 import 'package:locale_sheet/locale_sheet.dart';
-import 'package:locale_sheet/src/cli/logger.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
+import '../../test_helpers/logger.dart';
+
 class MockExporter extends Mock implements LocalizationExporter {}
-
-class TestLogger implements Logger {
-  final infos = <String>[];
-  final errors = <String>[];
-
-  @override
-  void info(String message) => infos.add(message);
-
-  @override
-  void error(String message) => errors.add(message);
-
-  @override
-  void infoOptions(Map<String, Object?> options) {
-    final joined = options.entries.map((e) => '${e.key}=${e.value}').join(', ');
-    infos.add('Options: $joined');
-  }
-
-  @override
-  void infoAvailableSheets(List<String> sheets) {
-    infos.add('Available sheets: ${sheets.join(', ')}');
-  }
-
-  @override
-  void infoSheetLocales(String sheetName, List<String> locales) {
-    infos.add('Sheet: $sheetName; Locales: ${locales.join(', ')}');
-  }
-
-  @override
-  void infoDefaultLocale(String locale) => infos.add('Default locale: $locale');
-
-  @override
-  void infoResult(String format, String outDir) => infos.add(
-    'Result: Success - "$format" format successfully written to $outDir.',
-  );
-
-  @override
-  void infoErrorResult(String message) => infos.add('Result: Error - $message');
-}
 
 class FakeParser extends ExcelParser {
   FakeParser(this.sheet);
